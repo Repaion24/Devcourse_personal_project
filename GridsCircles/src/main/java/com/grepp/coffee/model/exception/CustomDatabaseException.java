c9b9c147a27a8e6202a0ca2f14549a923664430e
Repaion24 <65394501+Repaion24@users.noreply.github.com>
2024-09-06 17:26:51 +0900

    Revert "Implement feature: 1"
    This reverts commit 50d7aae1bd71606a1b42a80e18251a39b1651239.

--- GridsCircles/src/main/java/com/grepp/coffee/model/exception/CustomDatabaseException.java
+++
@@ -1,7 +0,0 @@
-package com.grepp.coffee.model.exception;
-
-public class CustomDatabaseException extends RuntimeException {
-    public CustomDatabaseException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
