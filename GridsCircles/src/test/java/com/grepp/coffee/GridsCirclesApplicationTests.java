5a9626a1367ec4a4afafe1b480854318acb5fc49
Repaion24 <65394501+Repaion24@users.noreply.github.com>
2024-09-06 15:44:16 +0900

    Initial project setup

---
+++ GridsCircles/src/test/java/com/grepp/coffee/GridsCirclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grepp.coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridsCirclesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
