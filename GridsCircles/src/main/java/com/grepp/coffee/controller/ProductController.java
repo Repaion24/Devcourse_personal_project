a0c9fdda22d7d5826006e0bd26c067a7391cc8e7
Repaion24 <65394501+Repaion24@users.noreply.github.com>
2024-09-08 20:09:39 +0900

    Implement feature/#3

--- GridsCircles/src/main/java/com/grepp/coffee/controller/ProductController.java
+++ GridsCircles/src/main/java/com/grepp/coffee/controller/ProductController.java
@@ -4,14 +4,14 @@ import com.grepp.coffee.model.dto.ProductDTO;
 import com.grepp.coffee.model.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/coffee")
+@RequestMapping("/menu")
 public class ProductController {
     @Autowired
     private ProductService productService;
@@ -20,6 +20,17 @@ public class ProductController {
     @GetMapping("/list")
     public ResponseEntity<List<ProductDTO>> getAllProducts() {
         List<ProductDTO> products = productService.getAllProducts();
+        if (products.isEmpty()) {
+            return ResponseEntity.noContent().build(); // 리스트가 비어 있으면 204 No Content
+        }
+        return ResponseEntity.ok(products); // 200 OK와 함께 리스트 반환
+    }
+
+
+    @GetMapping("/{category}")
+    public ResponseEntity<List<ProductDTO>> getProductsByCategory(@PathVariable("category") String category) {
+        String[] categories = category.split(",");
+        List<ProductDTO> products = productService.getProductsByCategories(Arrays.asList(categories));
         if (products.isEmpty()) {
             return ResponseEntity.noContent().build(); // 리스트가 비어 있으면 204 No Content
         }
--- GridsCircles/src/main/java/com/grepp/coffee/model/repository/ProductRepository.java
+++ GridsCircles/src/main/java/com/grepp/coffee/model/repository/ProductRepository.java
@@ -2,11 +2,13 @@ package com.grepp.coffee.model.repository;
 
 import com.grepp.coffee.model.dto.ProductDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.sql.SQLException;
 import java.util.List;
 
 @Mapper
 public interface ProductRepository {
-    List<ProductDTO> getProducts();
+    List<ProductDTO> getAllProducts();
+    List<ProductDTO> findByCategories(@Param("categories") List<String> categories);
 }
